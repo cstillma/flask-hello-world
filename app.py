25ff8972779ed9d1b8d29bb6877b24a9551c8623
Caleb Stillman <cast9172@colorado.edu>
2023-07-28 19:32:03 +0000

    Database table creation route

--- app.py
+++ app.py
@@ -12,3 +12,22 @@ def testing():
     conn = psycopg2.connect("postgres://cstillma:wO7zKANdKLIl32xMMogTGB3yUiTqTNE7@dpg-cj218ri7l0fj36ub0f40-a/flaskappdb")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect("postgres://cstillma:wO7zKANdKLIl32xMMogTGB3yUiTqTNE7@dpg-cj218ri7l0fj36ub0f40-a/flaskappdb")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
+
+
